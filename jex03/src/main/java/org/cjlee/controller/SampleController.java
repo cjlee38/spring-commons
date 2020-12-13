de4788acdce59818df8321f32f16b4541816c411
cjlee <gptpem38@gmail.com>
2020-12-13 18:21:38 +0900

    ADD : default REST samplecontroller implemented IN jex03, P.365

--- jex03/src/main/java/org/cjlee/controller/SampleController.java
+++ jex03/src/main/java/org/cjlee/controller/SampleController.java
@@ -2,12 +2,16 @@ package org.cjlee.controller;
 
 import lombok.extern.log4j.Log4j;
 import org.cjlee.domain.SampleVO;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -36,7 +40,28 @@ public class SampleController {
     @GetMapping("/getList")
     public List<SampleVO> getList() {
         return IntStream.range(1, 10)
-                .mapToObj(i -> new SampleVO(i, i+" First", i+" Last"))
+                .mapToObj(i -> new SampleVO(i, i + " First", i + " Last"))
                 .collect(Collectors.toList());
     }
+
+    @GetMapping(value = "/getMap")
+    public Map<String, SampleVO> getmap() {
+        Map<String, SampleVO> map = new HashMap<>();
+        map.put("First", new SampleVO(111, "그루트", "주니어"));
+
+        return map;
+    }
+
+    @GetMapping(value = "/check", params = {"height", "weight"})
+    public ResponseEntity<SampleVO> check(Double height, Double weight) {
+
+        SampleVO vo = new SampleVO(0, "" + height, "" + weight);
+
+        return height < 150 ?
+                ResponseEntity.status(HttpStatus.BAD_GATEWAY).body(vo) :
+                ResponseEntity.status(HttpStatus.OK).body(vo);
+
+    }
+
+
 }
