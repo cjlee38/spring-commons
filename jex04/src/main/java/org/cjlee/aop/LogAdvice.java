09836d4b6c7c7ab78c80c413212d9dec6584085e
cjlee <gptpem38@gmail.com>
2020-12-16 18:43:39 +0900

    ADD : implemenet sample aop(before annotation) IN jex04, P.460

--- jex04/src/main/java/org/cjlee/aop/LogAdvice.java
+++ jex04/src/main/java/org/cjlee/aop/LogAdvice.java
@@ -1,6 +1,7 @@
 package org.cjlee.aop;
 
 import lombok.extern.log4j.Log4j;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -10,9 +11,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class LogAdvice {
 
-    @Before( "execution(* org.cjlee.service.SampleService*.*(..))")
+    @Before("execution(* org.cjlee.service.SampleService*.*(..))")
     public void logBefore() {
-        log.info("------------");
+        log.info("================");
+    }
+
+    @Before("execution(* org.cjlee.service.SampleService*.doAdd(String, String)) && args(str1, str2)")
+    public void logBeforeWithAParam(String str1, String str2) {
+        log.info("str1 = " + str1);
+        log.info("str2 = " + str2);
+    }
+
+    @AfterThrowing(pointcut = "execution(* org.cjlee.service.SampleService*.*(..))", throwing = "exception")
+    public void logException(Exception exception) {
+        log.info("Exception occured .. !");
+        log.info("exception = " + exception);
     }
 
 }
